9056f753160d7167e65adbe2591de8f1500c0bcc
HappyGirls <1738360311@qq.com>
2018-06-11 18:01:42 +0800

    项目2

--- js/myjs2.js
+++ js/myjs2.js
@@ -1,3 +1,12 @@
+/*
+* @Author: Marte
+* @Date:   2018-06-11 17:47:19
+* @Last Modified by:   Marte
+* @Last Modified time: 2018-06-11 17:48:26
+*/
+
+'use strict';
+
 $(function(){
     $(".flexslider").flexslider();
     $('.box').hover(function(){
@@ -8,7 +17,6 @@ $(function(){
     function(){
         $(this).find(".iconfont").removeClass('animated fadeInDown');
         $(this).find("p").removeClass('animated fadeInDown');
-    }
-    )
+    })
 
 })
